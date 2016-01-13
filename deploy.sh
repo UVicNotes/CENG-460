22684c002ed0f34954f6fee4a0ad5938071c0e88
Ben Hawker <ben@hawker.cc>
2016-01-13 14:28:22 -0800

    Improve travis push in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+LAST_COMMIT="$(git rev-parse --short HEAD)"
+
 # clone the old gh pages
 git clone "https://${GH_TOKEN}@${GH_REF}" site/
 cd site/
@@ -14,11 +16,11 @@ cd site/
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
-git config user.email "ben@hawker.me"
+git config user.email "travis.ci.build@gmail.com"
 
 # commit any new changes
 git add .
-git commit -m "Travis CI Deploying Github Pages"
+git commit -m "Travis CI - Deployed ${LAST_COMMIT} with MkDocs"
 
 # push to the repo
-git push "https://${GH_TOKEN}@${GH_REF}"
+git push --quiet "https://${GH_TOKEN}@${GH_REF}"
